38c4e3721564a91b6670e00178e4d329225dafed
touyou <fujiyou1121@gmail.com>
2025-05-31 10:47:37 +0900

    feat: add fnumber and ffproblem

---
+++ Sources/FFMultiplierCore/FFProblem.swift
@@ -0,0 +1,28 @@
+public struct FFProblem: Equatable, Hashable {
+    let a: FNumber
+    let b: FNumber
+    
+    public var answer: FNumber {
+        return a * b
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(a)
+        hasher.combine(b)
+    }
+}
+
+extension FFProblem {
+    /// 問題の生成
+    public static func generateProblem(for count: Int) -> [FFProblem] {
+        var problems: Set<FFProblem> = []
+        while problems.count < count {
+            let a = FNumber.random()
+            let b = FNumber.random()
+            let problem = FFProblem(a: a, b: b)
+            problems.insert(problem)
+        }
+        
+        return Array(problems).shuffled()
+    }
+}
---
+++ Sources/FFMultiplierCore/FNumber.swift
@@ -0,0 +1,34 @@
+/// 16進数を表す構造体
+public struct FNumber: Equatable, Hashable {
+    private let _value: Int
+    
+    /// 1の位の値
+    public var value0: Int {
+        return _value % 16
+    }
+    
+    /// 16の位の値
+    public var value1: Int {
+        return _value / 16
+    }
+    
+    public init(value: Int) {
+        assert(value >= 0, "FNumber value must be non-negative")
+        assert(value <= 15*15, "FNumber value must not exceed 15*15")
+        self._value = value
+    }
+    
+    public static func * (lhs: FNumber, rhs: FNumber) -> FNumber {
+        return FNumber(value: lhs._value * rhs._value)
+    }
+    
+    /// 乱数を生成する
+    public static func random() -> FNumber {
+        let value = Int.random(in: 0..<16)
+        return FNumber(value: value)
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(_value)
+    }
+}
