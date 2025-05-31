185edf95a4032ea477ab2c3ef883d55c22c34bce
touyou <fujiyou1121@gmail.com>
2025-05-31 12:00:21 +0900

    fix: actor error

--- Sources/FFMultiplierCore/GameModel.swift
+++ Sources/FFMultiplierCore/GameModel.swift
@@ -1,42 +1,53 @@
 /// ゲームのロジックを管理するアクター
 public actor GameModel {
-    public var problems: [FFProblem]
-    public var currentProblem: FFProblem?
-    public var score: Int
-    public var combo: Int
+    private var _problems: [FFProblem]
+    private var _currentProblem: FFProblem?
+    private var _score: Int
+    private var _combo: Int
+    
+    public var currentProblem: FFProblem? {
+        return _currentProblem
+    }
+    public var score: Int {
+        return _score
+    }
+    public var combo: Int {
+        return _combo
+    }
+        
     
     public init() {
-        self.problems = []
-        self.score = 0
-        self.combo = 0
+        self._problems = []
+        self._score = 0
+        self._combo = 0
     }
     
     /// ゲームを開始する
     public func startGame() {
-        self.problems = FFProblem.generateProblem(for: 1000)
-        self.score = 0
-        self.combo = 0
+        self._problems = FFProblem.generateProblem(for: 1000)
+        self._score = 0
+        self._combo = 0
         
-        self.currentProblem = pickNextProblem()
+        self._currentProblem = pickNextProblem()
     }
     
     private func pickNextProblem() -> FFProblem? {
-        guard !problems.isEmpty else { return nil }
-        return problems.popLast()
+        guard !_problems.isEmpty else { return nil }
+        return _problems.popLast()
     }
     
     /// 現在の問題に対する回答を行う
     public func answerCurrentProblem(with answer: FNumber) -> Bool {
-        guard let currentProblem = currentProblem else { return false }
+        guard let currentProblem = _currentProblem else { return false }
         let isCorrect = currentProblem.answer == answer
         if isCorrect {
-            score += ScoreSetting.acceptedPoints + ScoreSetting.calculateBonus(combo: combo)
-            combo += 1
+            _score += ScoreSetting.acceptedPoints + ScoreSetting.calculateBonus(combo: _combo)
+            _combo += 1
         } else {
-            score += ScoreSetting.failedPoints
-            combo = 0
+            _score += ScoreSetting.failedPoints
+            _combo = 0
         }
-        self.currentProblem = pickNextProblem()
+        self._currentProblem = pickNextProblem()
         return isCorrect
     }
 }
