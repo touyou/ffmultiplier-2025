487feae206243ad9c87da8597b8b55cb28aace83
touyou <465697+touyou@users.noreply.github.com>
2025-11-16 18:42:58 +0900

    Tabの採用とUUIDチェック

--- Sources/FFMultiplier2025/ContentView.swift
+++ Sources/FFMultiplier2025/ContentView.swift
@@ -13,31 +13,26 @@ struct ContentView: View {
     
     var body: some View {
         TabView(selection: $tab) {
-            NavigationStack {
-                WelcomeView(welcomeName: $welcomeName)
+            Tab("Home", systemImage: "house.fill", value: ContentTab.home) {
+                NavigationStack {
+                    WelcomeView(welcomeName: $welcomeName)
+                }
             }
-            .tabItem {
-                Label("Home", systemImage: "house.fill")
-            }
-            .tag(ContentTab.home)
             
-            NavigationStack {
-                RankingView()
-                    .navigationTitle(Text("Ranking"))
+            Tab("Ranking", systemImage: "star.fill", value: ContentTab.rankings) {
+                NavigationStack {
+                    RankingView()
+                        .navigationTitle(Text("Ranking"))
+                }
+                .toolbarTitleDisplayMode(.inlineLarge)
             }
-            .tabItem {
-                Label("Ranking", systemImage: "star.fill")
-            }
-            .tag(ContentTab.rankings)
             
-            NavigationStack {
-                SettingsView(appearance: $appearance, welcomeName: $welcomeName)
-                    .navigationTitle("Settings")
+            Tab("Settings", systemImage: "gearshape.fill", value: ContentTab.settings) {
+                NavigationStack {
+                    SettingsView(appearance: $appearance, welcomeName: $welcomeName)
+                        .navigationTitle("Settings")
+                }
             }
-            .tabItem {
-                Label("Settings", systemImage: "gearshape.fill")
-            }
-            .tag(ContentTab.settings)
         }
         .environment(viewModel)
         .preferredColorScheme(appearance == "dark" ? .dark : appearance == "light" ? .light : nil)
--- Sources/FFMultiplier2025/RankingView.swift
+++ Sources/FFMultiplier2025/RankingView.swift
@@ -2,49 +2,57 @@ import SwiftUI
 import FFMultiplierModel
 
 struct RankingView : View {
-  @Environment(RankingViewModel.self) var viewModel: RankingViewModel
-  @State internal var rankingList: OnlineRankingList? = nil
-  
-  var body: some View {
-    List {
-      if let rankingList, !rankingList.scores.isEmpty {
-        ForEach(rankingList.scores) { score in
-          RankItem(score: score)
+    @Environment(RankingViewModel.self) var viewModel: RankingViewModel
+    @State var rankingList: OnlineRankingList? = nil
+    
+    var body: some View {
+        List {
+            if let rankingList, !rankingList.scores.isEmpty {
+                ForEach(rankingList.scores) { score in
+                    RankItem(score: score)
+                }
+            } else {
+                Text("Empty")
+            }
         }
-      } else {
-        Text("Empty")
-      }
+        .task {
+            do {
+                let onlineRanking = try await FirebaseModel.shared.watchRanking()
+                self.rankingList = onlineRanking
+            } catch {
+                logger.error("error: \(error)")
+            }
+        }
     }
-    .task {
-      do {
-        let onlineRanking = try await FirebaseModel.shared.watchRanking()
-        self.rankingList = onlineRanking
-      } catch {
-        logger.error("error: \(error)")
-      }
-    }
-  }
 }
 
 struct RankItem: View {
-  let score: Score
-  @State internal var userName: String?
-  
-  var body: some View {
-    VStack(alignment: .leading) {
-      if let userName {
-        Text(userName.isEmpty ? "Anonymous" : userName)
-      } else {
-        ProgressView()
-      }
-      HStack {
-        Text("\(score.score)pt").bold()
-        Spacer()
-        Text("\(score.updatedAt, style: .date) \(score.updatedAt, style: .time)").font(.footnote)
-      }
+    let score: Score
+    @State var userName: String?
+    @State var userId: String?
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                if let userName {
+                    Text(userName.isEmpty ? "Anonymous" : userName)
+                } else {
+                    ProgressView()
+                }
+                if let userId {
+                    Text(userId).font(.footnote)
+                }
+            }
+            HStack {
+                Text("\(score.score)pt").bold()
+                Spacer()
+                Text("\(score.updatedAt, style: .date) \(score.updatedAt, style: .time)").font(.footnote)
+            }
+        }
+        .task {
+            let user = try? await score.getUser()
+            userName = user?.name
+            userId = user?.uuid
+        }
     }
-    .task {
-      userName = (try? await score.getUser())?.name
-    }
-  }
 }
